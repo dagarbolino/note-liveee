5f1f638a0dc976a4f82c68b83d5f5f3012f17097
alexandre dnk <dincq.alexandre@gmail.com>
2024-08-17 00:17:38 +0200

    chore: Update npm dependencies and add new components

---
+++ app/components/SheetContact.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+
+
+export default function SheetContact() {
+
+
+  return (
+    <Sheet>
+      <SheetTrigger>Open</SheetTrigger>
+      <SheetContent className="w-[400px] sm:w-[540px]">
+        <SheetHeader>
+          <SheetTitle>Are you absolutely sure?</SheetTitle>
+          <SheetDescription>
+            This action cannot be undone. This will permanently delete your account
+            and remove your data from our servers.
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  )
+}
--- app/dashboard/curriculums/curriculum/[id]/page.tsx
+++ app/dashboard/curriculums/curriculum/[id]/page.tsx
@@ -1,7 +1,11 @@
+import DrawerContact from "@/app/components/DrawerContact"
+import SheetContact from "@/app/components/SheetContact"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+
+
 import { Textarea } from "@/components/ui/textarea"
 import { getCurriculum, updateCurriculum } from "@/lib/actionsCurriculums"
 import Link from "next/link"
@@ -14,86 +18,93 @@ interface UpdatePageProps {
   params: Params
 }
 
-export default async function PageCurriculum({params}: UpdatePageProps) {
+export default async function PageCurriculum({ params }: UpdatePageProps) {
   const curriculum = await getCurriculum(params.id)
 
   return (
+    <>
+      <div className="mb-6 ml-6 border-l-4 border-orange-500 pl-6">
+      <SheetContact />
+      </div>
+
+      <div className="mb-6 ml-6 border-l-4 border-orange-500 pl-6">
+      <DrawerContact />
+      </div>
 
 
-    
+      <Card>
+        <form action={updateCurriculum}>
+          <Input type="hidden" name="id" value={curriculum?.id || ''} />
 
-    <Card>
-      <form action={updateCurriculum}>
-        <Input type="hidden" name="id" value={curriculum?.id || ''} />
-        
-        <CardHeader>
-          <CardTitle>Modifier un curriculum</CardTitle>
-          <CardDescription>Mettez à jour vos informations</CardDescription>
-        </CardHeader>
-        <CardContent className="flex flex-col gap-y-5 w-full">
+          <CardHeader>
+            <CardTitle>Modifier un curriculum</CardTitle>
+            <CardDescription>Mettez à jour vos informations</CardDescription>
+          </CardHeader>
+          <CardContent className="flex flex-col gap-y-5 w-full">
 
 
-          <div className="gap-y-2 flex flex-col w-60">
-            <Label className=" "  htmlFor="title">Titre</Label>
-            <Input className=" " defaultValue={curriculum?.title || ''} type="text" name="title" id="title" required />
-          </div>
-
-          <div className="gap-y-2 flex flex-col w-60">
-            <Label htmlFor="description">Description</Label>
-            <Textarea defaultValue={curriculum?.description || ''} name="description" id="description" required />
-          </div>
-
-          <div className="gap-y-2 flex flex-col w-60">
-            <Label htmlFor="last_name">Nom</Label>
-            <Input defaultValue={curriculum?.contact_details?.last_name || ''} type="text" name="last_name" id="last_name" required />
-          </div>
-
-          <div className="gap-y-2 flex flex-col w-60">
-            <Label htmlFor="first_name">Prénom</Label>
-            <Input defaultValue={curriculum?.contact_details?.first_name || ''} type="text" name="first_name" id="first_name" required />
+            <div className="gap-y-2 flex flex-col w-60">
+              <Label className=" " htmlFor="title">Titre</Label>
+              <Input className=" " defaultValue={curriculum?.title || ''} type="text" name="title" id="title" required />
             </div>
 
-          <div className="gap-y-2 flex flex-col w-60">
-            <Label htmlFor="postTitle">Titre du poste</Label>
-            <Input defaultValue={curriculum?.contact_details?.postTitle || ''} type="text" name="postTitle" id="postTitle" required />
-          </div>
+            <div className="gap-y-2 flex flex-col w-60">
+              <Label htmlFor="description">Description</Label>
+              <Textarea defaultValue={curriculum?.description || ''} name="description" id="description" required />
+            </div>
 
-          <div className="gap-y-2 flex flex-col w-60">
-            <Label htmlFor="email">Email</Label>
-            <Input defaultValue={curriculum?.contact_details?.email || ''} type="email" name="email" id="email" required />
-          </div>
+            <div className="gap-y-2 flex flex-col w-60">
+              <Label htmlFor="last_name">Nom</Label>
+              <Input defaultValue={curriculum?.contact_details?.last_name || ''} type="text" name="last_name" id="last_name" required />
+            </div>
 
-          <div className="gap-y-2 flex flex-col w-60">
-            <Label htmlFor="phone">Téléphone</Label>
-            <Input defaultValue={curriculum?.contact_details?.phone || ''} type="tel" name="phone" id="phone" required />
-          </div>
+            <div className="gap-y-2 flex flex-col w-60">
+              <Label htmlFor="first_name">Prénom</Label>
+              <Input defaultValue={curriculum?.contact_details?.first_name || ''} type="text" name="first_name" id="first_name" required />
+            </div>
 
-          <div className="gap-y-2 flex flex-col w-60">
-            <Label htmlFor="linkedin">LinkedIn</Label>
-            <Input defaultValue={curriculum?.contact_details?.linkedin || ''} type="url" name="linkedin" id="linkedin" />
-          </div>
+            <div className="gap-y-2 flex flex-col w-60">
+              <Label htmlFor="postTitle">Titre du poste</Label>
+              <Input defaultValue={curriculum?.contact_details?.postTitle || ''} type="text" name="postTitle" id="postTitle" required />
+            </div>
 
-          <div className="gap-y-2 flex flex-col w-60">
-            <Label htmlFor="photoUrl">Photo URL</Label>
-            <Input defaultValue={curriculum?.contact_details?.photoUrl || ''} type="url" name="photoUrl" id="photoUrl" />
-          </div>
+            <div className="gap-y-2 flex flex-col w-60">
+              <Label htmlFor="email">Email</Label>
+              <Input defaultValue={curriculum?.contact_details?.email || ''} type="email" name="email" id="email" required />
+            </div>
 
-          <div className="gap-y-2 flex flex-col w-60">
-            <Label htmlFor="completed">En attente || Complet</Label>
-            <Input defaultChecked={curriculum?.completed || false} type="checkbox" name="completed" id="completed" className="w-6 cursor-pointer" />
-          </div>
-        </CardContent>
+            <div className="gap-y-2 flex flex-col w-60">
+              <Label htmlFor="phone">Téléphone</Label>
+              <Input defaultValue={curriculum?.contact_details?.phone || ''} type="tel" name="phone" id="phone" required />
+            </div>
 
-        <CardFooter className="flex items-center justify-between">
-          <Button type="button" className="bg-red-500 hover:bg-red-600 text-white">
-            <Link href="/dashboard/curriculums">Annuler</Link>
-          </Button>
+            <div className="gap-y-2 flex flex-col w-60">
+              <Label htmlFor="linkedin">LinkedIn</Label>
+              <Input defaultValue={curriculum?.contact_details?.linkedin || ''} type="url" name="linkedin" id="linkedin" />
+            </div>
 
-          <Button type="submit" className="bg-orange-500 hover:bg-orange-600 text-white">
-            Modifier le curriculum
-          </Button>
-        </CardFooter>
-      </form>
-    </Card>
+            <div className="gap-y-2 flex flex-col w-60">
+              <Label htmlFor="photoUrl">Photo URL</Label>
+              <Input defaultValue={curriculum?.contact_details?.photoUrl || ''} type="url" name="photoUrl" id="photoUrl" />
+            </div>
+
+            <div className="gap-y-2 flex flex-col w-60">
+              <Label htmlFor="completed">En attente || Complet</Label>
+              <Input defaultChecked={curriculum?.completed || false} type="checkbox" name="completed" id="completed" className="w-6 cursor-pointer" />
+            </div>
+          </CardContent>
+
+          <CardFooter className="flex items-center justify-between">
+            <Button type="button" className="bg-red-500 hover:bg-red-600 text-white">
+              <Link href="/dashboard/curriculums">Annuler</Link>
+            </Button>
+
+            <Button type="submit" className="bg-orange-500 hover:bg-orange-600 text-white">
+              Modifier le curriculum
+            </Button>
+          </CardFooter>
+        </form>
+      </Card>
+    </>
   )
 }
